9d0ff44a2a0ae3c028a9f6d75b51d9d26881f4b1
zdd <29750121@qq.com>
2022-03-16 16:45:32 +0800

    例行提交

--- git.sh
+++ git.sh
@@ -6,5 +6,6 @@ echo '...'
 # https://blog.csdn.net/ABCisCOOL/article/details/109288845
 # read  -p "请输入提交信息Message: " message
 git add .
+# git commit -a -m "$message"
 git commit -a -m "例行提交"
 git push
