906f208a72c21116247eefc949aad1368296f2c5
zdd <29750121@qq.com>
2022-03-16 15:31:25 +0800

    例行提交[C

---
+++ git.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash
+echo 'exec >>> [vuepress-blog-oss git push]'
+echo '...'
+echo '...'
+
+# https://blog.csdn.net/ABCisCOOL/article/details/109288845
+read  -p "请输入提交信息Message: " message
+git add .
+git commit -a -m "$message"
+git push
